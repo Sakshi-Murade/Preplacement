fe5e157735cceb4a5d20c295d18308895449cbf0
Sakshi-Murade <147234168+Sakshi-Murade@users.noreply.github.com>
2024-08-08 21:33:56 +0530

    Create Majority_Elements.cpp

---
+++ Preplacement(DSA)/Majority_Elements.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+
+int majorityElement(vector<int>& nums) {
+    int candidate = 0;
+    int count = 0;
+
+    for (int num : nums) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+
+    count = 0;
+    for (int num : nums) {
+        if (num == candidate) {
+            count++;
+        }
+    }
+
+    if (count > nums.size() / 2) {
+        return candidate;
+    }
+
+    return -1; 
+}
+
+int main() {
+    vector<int> nums1 = {3, 2, 3};
+    vector<int> nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+    cout << "Majority Element in nums1: " << majorityElement(nums1) << endl;
+    cout << "Majority Element in nums2: " << majorityElement(nums2) << endl;
+
+    return 0;
+}
