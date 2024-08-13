8695f6ea828ba56722b66e00a834c92300b4f7af
Sakshi-Murade <147234168+Sakshi-Murade@users.noreply.github.com>
2024-08-13 22:18:08 +0530

    Create Intersection_of_two_linked_list.java

---
+++ Preplacement(DSA)/Intersection_of_two_linked_list.java
@@ -0,0 +1,59 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+        
+        while (ptrA != ptrB) {
+            ptrA = (ptrA == null) ? headB : ptrA.next;
+            ptrB = (ptrB == null) ? headA : ptrB.next;
+        }
+        
+        return ptrA;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       
+        ListNode a1 = new ListNode(4);
+        ListNode a2 = new ListNode(1);
+        ListNode a3 = new ListNode(8);
+        ListNode a4 = new ListNode(4);
+        ListNode a5 = new ListNode(5);
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
+        
+       
+        ListNode b1 = new ListNode(5);
+        ListNode b2 = new ListNode(6);
+        ListNode b3 = new ListNode(1);
+        b1.next = b2;
+        b2.next = b3;
+        b3.next = a3; 
+        
+       
+        Solution solution = new Solution();
+        ListNode intersection = solution.getIntersectionNode(a1, b1);
+        
+        if (intersection != null) {
+            System.out.println("Intersected at: " + intersection.val);
+        } else {
+            System.out.println("No intersection");
+        }
+    }
+}
